95bf49219767e20003e646c2857179e567acfc8f
Dydudu37d <lifuan666@gmail.com>
2025-10-31 23:59:38 +0800

    fix bug: import error

--- ALnn/__init__.py
+++ ALnn/__init__.py
@@ -3,6 +3,8 @@ __author__ = "Andylisg"
 __email__ = "lifuan666@gmail.com"
 
 from .nn import *
+import sys
+import os
 __all__ = ["nn","load_num_data","MiniCNN","MultiLogicNet"]
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
